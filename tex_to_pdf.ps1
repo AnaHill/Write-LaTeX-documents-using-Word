dc648617bfd9ee7b7d2081f32c6bf69a0d28e452
Antti Mäki <aj_maki@hotmail.com>
2022-02-07 05:36:03 +0200

    included tex to pdf .ps1 file

---
+++ tex_to_pdf.ps1
@@ -0,0 +1,19 @@
+# Document info
+$paper_name = 'WriteLaTeXusingWord'
+$Folder_path = $(get-location).Path + '\'; 
+## TEX to PDF
+Write-Host "Creating pdf"
+# bibtex.exe texbuildfiles/"BPM_sim_Draft" -include-directory=texbuildfiles
+bibtex.exe texbuildfiles/$paper_name -include-directory=texbuildfiles
+Write-Host "Bibliography created"
+# pdflatex.exe -synctex=1 -interaction=nonstopmode --aux-directory=texbuildfiles $output_filename_tex
+
+# pdflatex.exe -synctex=1 -interaction=nonstopmode --aux-directory=texbuildfiles $output_filename_tex
+pdflatex.exe -synctex=1 -interaction=nonstopmode --aux-directory=texbuildfiles --output-directory=texbuildfiles $output_filename_tex
+$output_fold_temp =  $Folder_path + 'texbuildfiles\' + $output_filename_pdf #
+Write-Host "pdf created"
+
+Write-Host "Moving $output_fold_temp"
+Write-Host "to $Folder_path"
+Move-Item -Path $output_fold_temp -Destination $Folder_path -Force
+Invoke-Item $output_filename_pdf
