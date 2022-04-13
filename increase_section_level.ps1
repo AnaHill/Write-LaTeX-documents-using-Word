6630f2c720f3a2a3d02fd51bbf2b0583d4277f96
Antti Mäki <aj_maki@hotmail.com>
2022-04-13 06:17:09 +0300

    optional increase section level added

---
+++ increase_section_level.ps1
@@ -0,0 +1,16 @@
+# This script will convert 
+    # \section --> \chapter
+    # \section* --> \chapter* (non numbered, e.g Abstract)
+    # \subsection --> \section
+    # \subsubsection --> \subsection
+    # Optional
+        # paragraph --> subsubsection
+    
+$modified_file_name = 'main_text.tex' 
+(gc $modified_file_name) | % {$_ -replace "\\section{", "\chapter{"} | sc $modified_file_name
+(gc $modified_file_name) | % {$_ -replace "\\section*{", "\chapter*{"} | sc $modified_file_name # abstract etc non numbered chapters
+(gc $modified_file_name) | % {$_ -replace "\\subsection{", "\section{"} | sc $modified_file_name 
+(gc $modified_file_name) | % {$_ -replace "\\subsubsection{", "\subsection{"} | sc $modified_file_name  
+# optional: convert \paragraph to \subsubsection and \subparagraph to \paragraph
+# (gc $modified_file_name) | % {$_ -replace "\\paragraph{", "\subsubsection{"} | sc $modified_file_name  # typically not used
+# (gc $modified_file_name) | % {$_ -replace "\\subparagraph{", "\paragraph{"} | sc $modified_file_name  # typically not used
